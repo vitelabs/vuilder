7a87f1a2c8f40fc53116d5c3700c858e6fff70c2
viteshan <wangjie@vite.org>
2022-05-31 14:36:22 +0800

    feat: start gvite with --virtual

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+SCRIPT_DIR=$(
+	cd $(dirname ${BASH_SOURCE[0]})
+	pwd
+)
 rm -rf $SCRIPT_DIR/ledger
 if [ $# == 1 ]; then
-BIN_FILE=$1
+	BIN_FILE=$1
 else
-BIN_FILE=gvite
+	BIN_FILE=gvite
 fi
 cd $SCRIPT_DIR
-exec ./$BIN_FILE --pprof > ./gvite.log
+exec ./$BIN_FILE --pprof --virtual >./gvite.log
